ff3d8884616998fca92ef47067342cf2070e8bbe
Lucas Prazeres <lucascprazeres@outlook.com>
2024-03-12 22:46:19 -0300

    adiciona exemplos de web server

---
+++ ch1/server3/lissajous/lissajous.go
@@ -0,0 +1,44 @@
+// Lissajous gera animações GIF de figuras de lissajous aleatórias
+package lissajous
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+)
+
+var pallete = []color.Color{color.Black, color.RGBA{0x32, 0xa8, 0x52, 0xff}}
+
+const (
+	blackIndex = 0 // primeira cor da paleta
+	greenIndex = 1 // próxima cor da paleta
+)
+
+func Lissajous(out io.Writer, cycles int) {
+	const (
+		// cycles  = 5     // número de revoluções completas do oscilador x
+		res     = 0.001 // resolução angular
+		size    = 100   // canvas da imagem cobre de [--size..+size]
+		nframes = 64    // número de quadros da animação
+		delay   = 8     // tempo entre quadros em unidades de 10ms
+	)
+	freq := rand.Float64() * 3.0 // frequência relativa do oscilador y
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, pallete)
+		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), greenIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTA: ignorando erros de codificação
+}
